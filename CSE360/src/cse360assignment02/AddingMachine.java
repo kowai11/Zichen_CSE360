63ffb532efd6b66bf8f160b338b3a42ae54a8886
a8433 <a8433@DESKTOP-VQ8MNLI>
2020-10-03 17:37:16 -0700

    Second version

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,36 @@
-/**package cse360assignment02;
+package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history;
+  
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total = total + value;
+	  history = history + " + " + Integer.toString(value);
   }
 
   public void subtract (int value) {
+	  total = total - value;
+	  history = history + " - " + Integer.toString(value);
   }
 
   public String toString () {
-    return "";
+	  
+    return "0" + history;
   }
 
   public void clear() {
+	  total = 0;
+	  history = "";
   }
-}**/
+}
